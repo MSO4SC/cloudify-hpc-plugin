e630bde9cb6bea0bd84968503539a32aa83a4e57
Javier Carnero <javier.carnero@atos.net>
2018-02-26 11:53:13 +0000

    Version 1.0

--- hpc_plugin/tests/workflow_tests.py
+++ hpc_plugin/tests/workflow_tests.py
@@ -283,3 +283,34 @@ class TestPlugin(unittest.TestCase):
                              True)
         else:
             logging.warning('[WARNING] Login could not be tested')
+
+    @workflow_test(os.path.join('blueprint',
+                                'blueprint_opm.yaml'),
+                   resources_to_copy=[(os.path.join('blueprint', 'hpc_plugin',
+                                                    'test_plugin.yaml'),
+                                       'hpc_plugin'),
+                                      (os.path.join('blueprint', 'scripts',
+                                                    'singularity_bootstrap_' +
+                                                    'run-flow-generic.sh'),
+                                       'scripts'),
+                                      (os.path.join('blueprint', 'scripts',
+                                                    'singularity_revert_run' +
+                                                    '-flow-generic.sh'),
+                                       'scripts')],
+                   inputs='set_inputs')
+    def test_opm_flow(self, cfy_local):
+        """ Install & Run workflows. """
+        cfy_local.execute('install', task_retries=0)
+        cfy_local.execute('run_jobs', task_retries=0)
+        cfy_local.execute('uninstall', task_retries=0)
+
+        # extract single node instance
+        instance = cfy_local.storage.get_node_instances()[0]
+
+        # due to a cfy bug sometimes login keyword is not ready in the tests
+        if 'login' in instance.runtime_properties:
+            # assert runtime properties is properly set in node instance
+            self.assertEqual(instance.runtime_properties['login'],
+                             True)
+        else:
+            logging.warning('[WARNING] Login could not be tested')
