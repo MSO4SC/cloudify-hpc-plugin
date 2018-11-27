1cf2da691803fff7de7c759a5aa7c54638a0548c
Javier Carnero <javier.carnero@atos.net>
2018-11-27 14:30:53 +0000

    Some bugs in test opm eosc solved

--- hpc_plugin/tests/blueprints/scripts/singularity_bootstrap_run-flow-generic.sh
+++ hpc_plugin/tests/blueprints/scripts/singularity_bootstrap_run-flow-generic.sh
@@ -8,19 +8,16 @@ IMAGE_NAME=$3
 
 # cd $CURRENT_WORKDIR ## not needed, already started there
 singularity pull --name $IMAGE_NAME $IMAGE_URI
-singularity pull --name remotelogger-cli.simg shub://sregistry.srv.cesga.es/mso4sc/remotelogger-cli:latest
 wget $REMOTE_URL
 ARCHIVE=$(basename $REMOTE_URL)
 tar zxvf $ARCHIVE
 DIRNAME=$(basename $ARCHIVE .tgz)
 DECK=$(ls $DIRNAME/*.DATA)
-BASEDECK=$(basename $DECK)
-PREFIXDECK="${BASEDECK%.*}"
-OUTPUTDIR=$(readlink -m simoutput)
 cat << EOF > run_generated.param
-deck_filename=$(readlink -m $CURRENT_WORKDIR)/$DECK
+ecl-deck-file-name=$(readlink -m $CURRENT_WORKDIR)/$DECK
 EOF
 
+singularity pull --name remotelogger-cli.simg shub://sregistry.srv.cesga.es/mso4sc/remotelogger-cli:latest
 mkdir -p simoutput
 JOB_LOG_FILTER_FILE='logfilter.yaml'
 cat << EOF > $JOB_LOG_FILTER_FILE
